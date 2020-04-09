e971f4fab1c268bfaa58df32015d0c0e012c436c
mobis <mobiskif@hotmail.com>
2020-04-09 12:40:05 +0300

    2

---
+++ src/Athom.java
@@ -0,0 +1,82 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Athom extends Component implements Runnable, MouseMotionListener {
+    private Image image;
+    private boolean isStarted=false;
+    int x0,y0;
+
+    public Athom(int x, int y) {
+        super();
+        setLocation(x, y);
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("res/athom.png");
+            image = ImageIO.read(is);
+            int diameter = 50;
+            image = image.getScaledInstance(diameter, diameter, Image.SCALE_SMOOTH);
+            setPreferredSize(new Dimension(diameter, diameter));
+            setSize(diameter, diameter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //new Thread(this).start();
+        //addActionListener(actionEvent -> System.out.println("---" + actionEvent));
+
+        addMouseMotionListener(this);
+
+        Athom a=this;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                isStarted=!isStarted;
+                if (isStarted) new Thread(a).start();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(image, 0, 0, this);
+        g.drawString("" + getX() + "," + getY(),  8, getHeight() / 2+6);
+    }
+
+    @Override
+    public void run() {
+        while (isStarted) {
+            //Dimension dXY = model.calculateDeltaXY(this);
+            //setLocation(dXY.width, dXY.height);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            getParent().repaint();
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        int x1,y1,dx,dy;
+        x1 = e.getX(); y1 = e.getY();
+        dx = x1 - x0; dy = y1 - y0;
+        Athom n = (Athom) e.getSource();
+        n.setBounds(n.getX() + dx, n.getY() + dy, n.getWidth(), n.getHeight());
+        n.getParent().repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        x0 = e.getX();
+        y0 = e.getY();
+    }
+}
---
+++ src/objectsPanel.java
@@ -0,0 +1,36 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class objectsPanel extends JPanel {
+    private Image image;
+    int w,h;
+
+    public objectsPanel() {
+        setLayout(null);
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("res/vdnh.jpg");
+            image = ImageIO.read(is);
+            image = image.getScaledInstance(240, 320, Image.SCALE_SMOOTH);
+            w=image.getWidth(this);
+            h=image.getHeight(this);
+            setPreferredSize(new Dimension(w, h));
+            setSize(w,h);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        add(new Athom(50,50));
+        add(new Athom(150,150));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+        g.drawRect(1,1,getWidth()-2,getHeight()-2);
+        g.drawString(getWidth()+","+getHeight(),16,16);
+        g.drawString(w+","+h,16,32);
+    }
+}
