19ccfe1532038c12103a46f0cfd258c3a3d9464b
mobis <mobiskif@hotmail.com>
2020-04-09 09:21:11 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet {
+    public Main() throws HeadlessException {
+        super();
+        setLayout( new BorderLayout());
+        add(new MainForm().$$$getRootComponent$$$());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("ВДНХ");
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.add(new MainForm().$$$getRootComponent$$$());
+        f.pack();
+        f.setVisible(true);
+    }
+}
