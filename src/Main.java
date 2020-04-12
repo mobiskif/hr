7ece7a898cf941058a921af731fc33b4fa3f2c43
john <mobiskif@gmail.com>
2020-04-12 08:04:46 +0300

    10/04/20

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import javax.media.format.VideoFormat;
 import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
+import java.util.Vector;
+import javax.media.*;
 
 public class Main extends Applet {
+    private static Player player;
+
     public Main() throws HeadlessException {
         super();
         setLayout( new BorderLayout());
@@ -18,6 +23,23 @@ public class Main extends Applet {
         f.pack();
         f.setVisible(true);
 
-
+        JPanel panel = (JPanel) f.getContentPane();
+        Vector devicesList = CaptureDeviceManager.getDeviceList(new VideoFormat("YUV"));
+        System.out.println(devicesList);
+        //String mediaFile = "vfw:Microsoft WDM Image Capture (Win32):0";
+        String mediaFile = "vfl://:0";
+        try {
+            MediaLocator mlr = new MediaLocator(mediaFile);
+            player = Manager.createRealizedPlayer(mlr);
+            player.setRate(100);
+            if (player.getVisualComponent() != null) {
+                panel.add("Center", player.getVisualComponent());
+            }
+            if (player.getControlPanelComponent() != null) {
+                panel.add("South", player.getControlPanelComponent());
+            }
+        } catch (Exception e) {
+            System.err.println("Got exception " + e);
+        }
     }
 }
